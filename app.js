d6950eb478f371989c7811c24caada81dd5dc0c3
Lloyd Sibson <lloydsibson@gmail.com>
2025-06-08 19:48:49 +0100

    Dev: Added Countdown Timer for Remaster release

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 // FOOTER - CURRENT YEAR
 const currentYear = new Date().getFullYear();
 document.querySelector("#currentYear").textContent = currentYear;
+
+////////////////////////////////
+// COUNTER DOWN TIMER
+////////////////////////////////
+const countdownDate = new Date("June 26, 2025 23:59:59").getTime();
+const timer = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("countdown").innerHTML =
+      '<div class="countdown-breakdown-container"><div class="countdown-days"><span>Out Now</span></div></div>';
+    //document.getElementById("countdown").style.display = "none";
+    return;
+  }
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById(
+    "countdown"
+  ).innerHTML = `<div class="countdown-breakdown-container">
+<div class="countdown-days">
+<span>${days}</span>Days</div>:<div class="countdown-hours">
+<span>${hours}</span>Hours</div>:<div class="countdown-mins">
+<span>${minutes}</span>Mins</div>:<div class="countdown-seconds">
+<span>${seconds}</span>secs</div>
+</div>`;
+}, 1000);
+////////////////////////////////
+// COUNT DOWN TIMER ENDS
+////////////////////////////////
